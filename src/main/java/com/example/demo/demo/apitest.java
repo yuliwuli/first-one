b0fa8e3e88b3cc5bf2095656c4835392abc63ddb
yunliwuli <applehumingming@163.com>
2020-07-13 13:22:10 +0800

    apitest

---
+++ src/main/java/com/example/demo/demo/apitest.java
@@ -0,0 +1,18 @@
+package com.example.demo.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2020/7/13.
+ */
+
+@RestController
+public class apitest {
+    final String result = "欢迎光临";
+
+    @RequestMapping(value = "/test")
+    public String test(){
+        return result;
+    }
+}
